512b3ca97170302a8ebc38888cf1307697874ddc
KGB-TC <tochukwuchrisejiofor@gmail.com>
2022-11-25 16:29:52 -0800

    Team Work

--- _printf.c
+++ _printf.c
@@ -1,59 +1,101 @@
 #include "main.h"
 
-
+/**
+ * File: _printf.c
+ *
+ * Auth: Theodore Chris-Ejiofor & Harris Okatta
+ */
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 
 /**
- * _printf - formatted output conversion and print data.
+ * cleanup - Peforms cleanup operations for _printf.
  *
- * @format: input string.
+ * @args: A va_list of arguments provided to _printf.
  *
- * Return: number of chars printed.
+ * @output: A buffer_t struct.
  */
-
-int _printf(const char *format, ...)
-
+void cleanup(va_list args, buffer_t *output)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+/**
+ * run_printf - Reads through the format string for _printf.
+ *
+ * @format: Character string to print - may contain directives.
+ *
+ * @output: A buffer_t struct containing a buffer.
+ *
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
 	{
-		if (format[i] == '%')
+		len = 0;
+		if (*(format + i) == '%')
 		{
-			if (format[i + 1] == '\0')
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
 			{
-				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
 			}
-			else
+			else if (*(format + i + tmp + 1) == '\0')
 			{
-				function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
+				ret = -1;
+				break;
 			}
-			i++;
 		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-	return (len);
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ *
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *output;
+	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
 }
